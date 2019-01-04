dd6ef6ad2d7682eaf44b3e8ed655e8b922bf372e
Robert Masen <r@robertmasen.pizza>
2019-01-03 22:28:22 -0600

    add new example

---
+++ examples/js_to_ron.rs
@@ -0,0 +1,21 @@
+extern crate ressa;
+use ressa::*;
+
+fn main() {
+    let mut args = ::std::env::args();
+    let _ = args.next().unwrap();
+    let path = args.next().expect("One argument required");
+    let js = ::std::fs::read_to_string(path).expect("Failed to read path");
+    let module = if let Some(flag) = args.next() {
+        flag.ends_with("m") || flag.ends_with("module")
+    } else {
+        false
+    };
+    let mut p = Builder::new()
+                .module(module)
+                .js(js)
+                .build()
+                .unwrap();
+    let ast = p.parse().unwrap();
+    println!("{:#?}", ast);
+}
