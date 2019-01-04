7c2b3d3fe299d4c3049df346b47180bc8baa3e9c
Robert Masen <r@robertmasen.pizza>
2019-01-03 22:28:14 -0600

    rename example variable

--- examples/simple_module.rs
+++ examples/simple_module.rs
@@ -4,6 +4,6 @@ fn main() {
     let js = include_str!("simple.mjs");
     let mut builder = Builder::new();
     let mut p = builder.module(true).tolerant(false).js(js).build().unwrap();
-    let script = p.parse().unwrap();
-    println!("{:#?}", script);
+    let module = p.parse().unwrap();
+    println!("{:#?}", module);
 }
