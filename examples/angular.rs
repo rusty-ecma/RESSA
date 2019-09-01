77fd16997d1a2d7fa8e3449bb1663da688d6f72f
Robert <r@robertmasen.com>
2019-09-01 15:44:22 -0500

    more iterations on profilable benchmark

--- examples/angular.rs
+++ examples/angular.rs
@@ -4,8 +4,10 @@ use ressa::Parser;
 
 fn main() {
     let js = include_str!("../node_modules/angular/angular.js");
-    for item in Parser::new(js).unwrap() {
-        let unwrapped = item.unwrap();
-        ::std::mem::forget(unwrapped);
+    for _ in 0..1000 {
+        for item in Parser::new(js).unwrap() {
+            let unwrapped = item.unwrap();
+            ::std::mem::forget(unwrapped);
+        }
     }
 }
