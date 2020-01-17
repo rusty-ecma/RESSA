3f4d7c245137c03245d1d0392e2cf254a105d829
Robert <r@robertmasen.com>
2020-01-17 16:02:49 -0600

    clean up some of the line-number code

--- tests/test262/addLineNumbers.js
+++ tests/test262/addLineNumbers.js
@@ -3,20 +3,20 @@ function addNumbers() {
     let lineCount = codeEl.textContent.split('\n').length;
     let gutterWidth = lineCount > 99 ? 50 : 25;
     const gutter = document.createElement('div');
-    gutter.setAttribute('class', 'code-gutter')
-    document.body.appendChild(gutter)
+    gutter.setAttribute('class', 'code-gutter');
+    document.body.appendChild(gutter);
     gutter.style.top = codeEl.offsetTop;
     gutter.style.height = codeEl.clientHeight;
     gutter.style.top = codeEl.offsetTop;
     gutter.style.left = codeEl.offsetLeft - gutterWidth;
     gutter.style.width = `${gutterWidth}px`;
-    for (var i = 0; i < lineCount; i++) {
-        let num = document.createElement('span')
+    for (var i = 0; i < lineCount - 1; i++) {
+        let num = document.createElement('span');
         num.innerText = `${i + 1}.`;
         num.classList.add('gutter-number');
         gutter.appendChild(num);
     }
-};
+}
 function highlight() {
     const blocks = document.querySelectorAll('pre code')
     for (const block of blocks) {
